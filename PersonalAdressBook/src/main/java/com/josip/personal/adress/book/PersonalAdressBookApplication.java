d1a5f0ffe26db66c6ccc17a21d875ff6aac71d0c
Josip Bošnjak <jbosnjak@unipu.hr>
2019-05-19 16:35:55 +0200

    dodan projekt

---
+++ PersonalAdressBook/src/main/java/com/josip/personal/adress/book/PersonalAdressBookApplication.java
@@ -0,0 +1,13 @@
+package com.josip.personal.adress.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAdressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAdressBookApplication.class, args);
+	}
+
+}
