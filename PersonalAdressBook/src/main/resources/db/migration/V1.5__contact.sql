793c735d95e9bf7459c20679e102677e263129b1
Josip Bošnjak <jbosnjak@unipu.hr>
2019-05-20 17:41:39 +0200

    added
    Created table contact

---
+++ PersonalAdressBook/src/main/resources/db/migration/V1.5__contact.sql
@@ -0,0 +1,19 @@
+CREATE TABLE contact
+(
+    id bigserial NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    phone varchar(13) NOT NULL,
+    email character varying(50),
+    sex_id bigint NOT NULL,
+    address_id bigint NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_sex FOREIGN KEY (sex_id)
+        REFERENCES sex (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT fk_address FOREIGN KEY (address_id)
+        REFERENCES address (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
