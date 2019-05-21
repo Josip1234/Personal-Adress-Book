c42cabbd5d52f2b115cab0345110a1dc5c8b01a3
Josip Bošnjak <jbosnjak@unipu.hr>
2019-05-21 14:32:34 +0200

    update, added
    Updated function which add sex values to the list added random funcion and java doc comments. Initialized list in main class given 5 values to be inserted in the list then printed by the function. Added function which print all values as string.

--- PersonalAdressBook/src/test/java/com/josip/personal/adress/book/DataEntry.java
+++ PersonalAdressBook/src/test/java/com/josip/personal/adress/book/DataEntry.java
@@ -2,6 +2,7 @@ package com.josip.personal.adress.book;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 
@@ -12,31 +13,66 @@ import java.util.List;
  * @version 1.0
  * @since 21.5.2019. 10:26
  * 
+ * 
  * This class inserts random generated data into the migrate file which 
- * inserts data into database. Also it checks if migration file name already exsists.
+ * inserts data into database. Also it checks if migration file name already exists.
  * If it exists, it will change random version of that file by adding one to the version.
- * If that version exsists too, it will add until there is available. Also, 
+ * If that version e too, it will add until there is available. Also, 
  * it contains functions which generates random data.
  */
 
 public class DataEntry {
 	/**
-	 * 
+	 * @since 21.5.2019. 14:00
 	 * @param numberOfRecords
 	 * @return generated list of sex which depends on number of records
 	 * 
-	 * This function generates random male female m or f data
+	 * <i>This function generates random male female m or f data
 	 * which depends on number of records.
+	 * </i>
+	 * <h2>How this algorithm works</h2>
+	 * <strong>
+	 * First, initialize array and add some values.
+	 * Initialize list as array list, because it can be extended,
+	 * it is very useful in this case to use array list, not linked list or
+	 * array. 
+	 * Initialize random values.
+	 * While number is smaller than number of records 
+	 * get random index include last element of array.
+	 * Add value to the list. 
+	 * Then return random list of sex.
+	 * </strong>
 	 */
     public static List<String> generateSex(int numberOfRecords) {
     	String[] combinations= new String[]{"male","female","m","f"};
     	List<String> sexList=new ArrayList<String>();
+    	Random random=new Random();
+    	for(int number=0;number<numberOfRecords;number++) {
+    		int index=random.nextInt(combinations.length);
+    		sexList.add(combinations[index]);
+    	}
     	return sexList;
     	
     }
+    /***
+     * @author Josip Bošnjak
+     * @param list
+     * @since 21.5.2019 14:22
+     * 
+     * <h2>Description</h2>
+     * <strong>For each element type string in the list
+     * print element by element in the list.</strong>
+     */
+    public static void printList(List<String> list) {
+    	for (String string : list) {
+			System.out.println(string);
+		}
+    }
 	public static void main(String[] args) {
-	
-
+	 
+		final int generate=5;
+		List<String> generateList=generateSex(generate);
+        printList(generateList);
 	   
 	   
 	   
