3ce3fc23273544d5a14c873e856fa9b78d62e6aa
Josip Bošnjak <jbosnjak@unipu.hr>
2019-05-24 14:18:40 +0200

    added, updated
    Added comments in controller added list of data into the web site, and updated template name.

--- PersonalAdressBook/src/main/java/com/josip/personal/address/book/controllers/TemplateController.java
+++ PersonalAdressBook/src/main/java/com/josip/personal/address/book/controllers/TemplateController.java
@@ -2,6 +2,9 @@ package com.josip.personal.address.book.controllers;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +15,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.josip.personal.address.book.data.access.layer.SexRepository;
+import com.josip.personal.address.book.presentation.layer.Sex;
 
+/****
+ * 
+ * @author Josip Bošnjak
+ * @since 24.5.2019 14:10
+ * @version 1.0
+ * <h2>Description</h2>
+ * <strong>
+ * This class receive request and send requests.
+ * Data is sent to user view by repository.
+ * First, list is generated and repository is used to 
+ * receive data from database. 
+ * Then, lambda expression is used to add data to the list.
+ * List is added to the model then, which controller will
+ * pass to the web site. User will see that data at the 
+ * site.
+ * </strong>
+ */
 @Controller
 public class TemplateController {
 
@@ -23,10 +44,12 @@ public class TemplateController {
 		this.sexRepository=sexRepository;
 	}
 	
-	 @GetMapping({"/", "/test"})
-	    public String test() {
-	        
-	        return "test";
+	 @GetMapping({"/", "/template"})
+	    public String template(Model model) {
+	        List<Sex> sex = new ArrayList<>();
+	        sexRepository.findAll().forEach(i->sex.add(i));
+	        model.addAttribute(sex);
+	        return "template";
 	    }
 
 	
