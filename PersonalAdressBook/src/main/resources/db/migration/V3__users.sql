474cc41208e36043acc0a1bb4aca7da4aaf4c395
Josip Bošnjak <jbosnjak@unipu.hr>
2019-05-31 18:34:28 +0200

    created, updated
    created table for users updated sql script vharacter varying changed to varchar althought it is the same

---
+++ PersonalAdressBook/src/main/resources/db/migration/V3__users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users
+(
+    id bigserial NOT NULL,
+    firstName varchar(70) NOT NULL,
+    lastName varchar(150) NOT NULL,
+    age integer,
+    email varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    role varchar(20) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT email UNIQUE (email)
+
+)
