de606e83df84989d5801c05ea069fb889aead27a
Josip Bošnjak <jbosnjak@unipu.hr>
2019-06-02 01:06:16 +0200

    updated
    updated sql file changed size of password characters

--- sql/user.sql
+++ sql/user.sql
@@ -5,7 +5,7 @@ CREATE TABLE users
     lastName varchar(50) NOT NULL,
     age integer,
     email varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
+    password varchar(150) NOT NULL,
     role varchar(20) NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT email UNIQUE (email)
