76ec115a0856c4e3f9d60048e434444941073d13
Josip Bošnjak <jbosnjak@unipu.hr>
2019-06-02 18:27:34 +0200

    optimised imports

--- PersonalAdressBook/src/main/java/com/josip/personal/address/book/controllers/RegistrationController.java
+++ PersonalAdressBook/src/main/java/com/josip/personal/address/book/controllers/RegistrationController.java
@@ -7,11 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.josip.personal.address.book.data.access.layer.UserRepository;
-import com.josip.personal.address.book.presentation.layer.Sex;
 import com.josip.personal.address.book.presentation.layer.User;
 
 @Controller
