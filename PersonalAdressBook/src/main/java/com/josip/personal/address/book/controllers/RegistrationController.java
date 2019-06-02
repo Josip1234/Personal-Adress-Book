3ce05d18760bcee1be469bbfe86081645cd7f816
Josip Bošnjak <jbosnjak@unipu.hr>
2019-06-02 16:37:43 +0200

    added
    added function in register controller to return login if successfully registered user

--- PersonalAdressBook/src/main/java/com/josip/personal/address/book/controllers/RegistrationController.java
+++ PersonalAdressBook/src/main/java/com/josip/personal/address/book/controllers/RegistrationController.java
@@ -3,6 +3,7 @@ package com.josip.personal.address.book.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.josip.personal.address.book.data.access.layer.UserRepository;
 
@@ -15,5 +16,9 @@ public class RegistrationController {
 		this.userRepository=userRepository;
 	}
 
+	@PostMapping("/register/registerUser")
+	public String register() {
+		return "/login";
+	}
 	
 }
